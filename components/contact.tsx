a2332894d5b9e940d8c8a5abe05d8d071265dff5
raflisbk <54540217+raflisbk@users.noreply.github.com>
2024-12-08 19:46:46 +0700

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button';
-import { Github, Mail, Linkedin } from 'lucide-react';
-import Link from 'next/link';
+import { Button } from '@/components/ui/button'
+import { Github, Mail, Linkedin } from 'lucide-react'
+import Link from 'next/link'
 
 export function Contact() {
   return (
@@ -8,43 +8,35 @@ export function Contact() {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-6 text-center">Hubungi Saya</h2>
         <p className="text-center text-muted-foreground mb-8"></p>
+        
         <div className="flex justify-center space-x-6 mb-8">
-          {/* Tombol GitHub */}
           <Button variant="outline" size="icon" asChild>
             <Link href="https://github.com/raflisbk" target="_blank">
               <Github className="h-5 w-5" />
               <span className="sr-only">GitHub</span>
             </Link>
           </Button>
-
-          {/* Tombol LinkedIn */}
+          
           <Button variant="outline" size="icon" asChild>
-            <a
-              href="https://www.linkedin.com/in/mohamad-rafli-agung-subekti-640a77285/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="https://www.linkedin.com/in/mohamad-rafli-agung-subekti-640a77285/" target="_blank" rel="noopener noreferrer">
               <Linkedin className="h-5 w-5" />
               <span className="sr-only">LinkedIn</span>
             </a>
           </Button>
-
-          {/* Tombol Mail */}
+          
+          {/* Tombol Email */}
           <Button variant="outline" size="icon" asChild>
-            <a
-              href="mailto:raflisbk@gmail.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href="mailto:raflisbk@gmail.com" target="_blank" rel="noopener noreferrer">
               <Mail className="h-5 w-5" />
-              <span className="sr-only">Mail</span>
+              <span className="sr-only">Email</span>
             </a>
           </Button>
         </div>
+        
         <p className="text-center text-muted-foreground">
           Kota Pasuruan, Indonesia
         </p>
       </div>
     </section>
-  );
+  )
 }
