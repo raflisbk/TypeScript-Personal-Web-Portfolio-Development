8590a4dcf226177e7184b378e3ccaeaccee1631b
raflisbk <54540217+raflisbk@users.noreply.github.com>
2024-12-08 17:22:14 +0700

    Update projects.tsx

--- components/projects.tsx
+++ components/projects.tsx
@@ -81,7 +81,7 @@ export function Projects() {
             d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" 
           />
         </svg>
-        Tidak ada proyek yang ditemukan.
+        Yah kena limit API.
       </div>
     )
   }
