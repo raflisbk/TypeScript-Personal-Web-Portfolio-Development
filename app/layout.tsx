5b84e63b245ddc30ccdf75c578718d1ef887ed89
raflisbk <54540217+raflisbk@users.noreply.github.com>
2024-12-09 13:08:09 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={cn(inter.className, "min-h-screen bg-background antialiased futuristic-bg")}>
         <ThemeProvider
           attribute="class"
