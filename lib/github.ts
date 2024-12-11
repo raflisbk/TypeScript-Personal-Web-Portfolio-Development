e220d13e873339987a6fd2ea0dd57d99c5afab47
raflisbk <54540217+raflisbk@users.noreply.github.com>
2024-12-11 21:12:59 +0700

    Update github.ts

--- lib/github.ts
+++ lib/github.ts
@@ -58,7 +58,7 @@ export async function getGitHubProjects(username: string): Promise<Project[]> {
     const response = await fetchWithRetry(() => octokit.repos.listForUser({
       username,
       sort: 'created',
-      direction: 'asc',
+      direction: 'desc',
       per_page: 5
     }));
 
